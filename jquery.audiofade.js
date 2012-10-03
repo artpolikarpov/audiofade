9ec1e418d311b8b2ff5b992d71798c197cbc32bb
Artem Polikarpov <artpolikarpov@gmail.com>
2012-10-04 02:09:42 +0400

    Ссылка для закачки, чейнинг, исправленый audioFadeTo

--- jquery.audiofade.js
+++ jquery.audiofade.js
@@ -4,28 +4,38 @@
 (function ($) {
   var _options = {
     time: 333,
-    volume: null
+    volume: null,
+    onComplete: null
   }
   $.fn.audioFadeOut = function (options) {
     options = $.extend({}, _options, options);
+    console.log(options);
     this.each(function () {
       $(this).stop().animate({volume: 0}, options.time, function () {
         this.pause();
       });
     });
+    return this;
   }
   $.fn.audioFadeIn = function (options) {
     options = $.extend({}, _options, options);
+    console.log(options);
     this.each(function () {
       this.play();
       $(this).stop().animate({volume: 1}, options.time);
     });
+    return this;
   }
   $.fn.audioFadeTo = function (options) {
     options = $.extend({}, _options, options);
-    this.each(function (options) {
-      this.play();
-      $(this).stop().animate({volume: options.volume}, options.time);
+    console.log(options);
+    this.each(function () {
+      $(this).stop().animate({volume: options.volume}, options.time, function () {
+        if (options.onComplete) {
+          options.onComplete.call(this);
+        }
+      });
     });
+    return this;
   }
 })(jQuery);
